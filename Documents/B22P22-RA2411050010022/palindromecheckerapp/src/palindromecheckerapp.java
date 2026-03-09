5bbfec2953720bccd3d1c7e64640c1f62f86665d
snehalathakoka <snehalathakoka12@gmail.com>
2026-03-09 15:21:19 +0530

    UC6

--- Documents/B22P22-RA2411050010022/palindromecheckerapp/src/palindromecheckerapp.java
+++ Documents/B22P22-RA2411050010022/palindromecheckerapp/src/palindromecheckerapp.java
@@ -1,24 +1,19 @@
 import java.util.Stack;
-
-public class PalindromeCheckerApp {
+public class palindromecheckerapp {
 
     public static void main(String[] args) {
 
-        // Declare and initialize the input string
+
         String input = "noon";
 
-        // Create a stack to store characters
         Stack<Character> stack = new Stack<>();
 
-        // Push each character into the stack
         for (char c : input.toCharArray()) {
             stack.push(c);
         }
 
-        // Assume it is a palindrome
         boolean isPalindrome = true;
 
-        // Compare original string with reversed (stack pop)
         for (char c : input.toCharArray()) {
             if (c != stack.pop()) {
                 isPalindrome = false;
@@ -26,7 +21,7 @@ public class PalindromeCheckerApp {
             }
         }
 
-        // Display result
+
         if (isPalindrome) {
             System.out.println(input + " is a palindrome.");
         } else {
