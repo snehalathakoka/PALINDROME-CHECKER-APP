79c2745dc4b52005629972b7ef77de080a04de6c
snehalathakoka <snehalathakoka12@gmail.com>
2026-03-24 12:33:25 +0530

    UC8

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String input = "level";
+
+        LinkedList<Character> list = new LinkedList<>();
+
+        for (char c : input.toCharArray()) {
+            list.add(c);
+        }
+
+        boolean isPalindrome = true;
+
+        while (list.size() > 1) {
+            if (list.removeFirst() != list.removeLast()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
